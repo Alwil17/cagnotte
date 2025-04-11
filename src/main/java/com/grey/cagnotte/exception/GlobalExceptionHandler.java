fa72bbda1b0f7cd9a6b08f64b9cd61c387d51331
alwil17 <willialfred24@gmail.com>
2025-04-11 01:00:12 +0000

    Activated EnableMethodSecurity

--- src/main/java/com/grey/cagnotte/exception/GlobalExceptionHandler.java
+++ src/main/java/com/grey/cagnotte/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.grey.cagnotte.exception;
 import com.grey.cagnotte.payload.response.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,6 +14,12 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Object> handleUsernameAlreadyTaken(CagnotteCustomException ex) {
         ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), ex.getErrorCode());
         return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<?> handleAccessDeniedException(AccessDeniedException ex) {
+        ErrorResponse errorResponse = new ErrorResponse("You don't have permissions to access this resource.","FORBIDDEN_ACCESS_DENIED");
+        return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
     }
 }
 
